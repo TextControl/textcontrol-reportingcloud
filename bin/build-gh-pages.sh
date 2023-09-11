3064d67bb489a522f0687eb7a8f0151b22296efd
Jonathan Maron <jonathan.maron@theimagingsource.com>
2023-09-11 10:37:31 +0200

    Updated API documentation builder

--- bin/build-gh-pages.sh
+++ bin/build-gh-pages.sh
@@ -1,62 +1,43 @@
 #!/usr/bin/env bash
 
-RC_PATH=~/lib/textcontrol/textcontrol-reportingcloud
-GH_PATH=~/lib/textcontrol/textcontrol-reportingcloud-gh
+LIB_PATH_COMPOSER=~/.composer/vendor/bin
+LIB_PATH_PACKAGE=~/lib/textcontrol/textcontrol-reportingcloud
+LIB_PATH_BRANCH=~/lib/textcontrol/textcontrol-reportingcloud-gh
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-rm -fr ${GH_PATH}
+rm $LIB_PATH_COMPOSER/phpdoc
 
-git clone git@github.com:TextControl/textcontrol-reportingcloud.git --branch gh-pages ${GH_PATH}
+wget --quiet https://github.com/phpDocumentor/phpDocumentor/releases/download/v3.4.1/phpDocumentor.phar \
+      -O $LIB_PATH_COMPOSER/phpdoc
+
+chmod +x $LIB_PATH_COMPOSER/phpdoc
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-# download phar from https://github.com/phpDocumentor/phpDocumentor/releases
-# and copy to ~/.composer/vendor/bin/phpdoc
+rm -fr $LIB_PATH_BRANCH
 
-rm -fr ${GH_PATH}/docs-api
+git clone git@github.com:TextControl/textcontrol-reportingcloud.git --branch gh-pages $LIB_PATH_BRANCH
 
-mkdir -p ${GH_PATH}/docs-api
+# ----------------------------------------------------------------------------------------------------------------------
 
-~/.composer/vendor/bin/phpdoc run --cache-folder /tmp/ --directory ${RC_PATH}/src --target ${GH_PATH}/docs-api --template clean
+rm -fr $LIB_PATH_BRANCH/docs-api
 
-cd ${GH_PATH} || exit
+mkdir -p $LIB_PATH_BRANCH/docs-api
+
+$LIB_PATH_COMPOSER/phpdoc run            \
+    --cache-folder /tmp/                 \
+    --directory ${LIB_PATH_PACKAGE}/src  \
+    --target $LIB_PATH_BRANCH/docs-api   \
+    --template clean
+
+cd $LIB_PATH_BRANCH || exit
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 git add .
 
 git commit -am"Updated API documentation"
-
-git push origin gh-pages
-
-# ----------------------------------------------------------------------------------------------------------------------
-
-cd ${RC_PATH} || exit
-
-rm -fr ${GH_PATH}/test-coverage
-
-mkdir -p ${GH_PATH}/test-coverage
-
-./vendor/bin/phpunit --coverage-html ${GH_PATH}/test-coverage
-
-cd ${GH_PATH}/test-coverage || exit
-
-# GitHub pages will not serve
-# from hidden directories,
-# hence we rename them
-mv .css   css
-mv .js    js
-mv .icons icons
-
-# and update all HTML files
-find * -name '*.html' | xargs perl -pi -e "s/\.css\//css\//sig;"
-find * -name '*.html' | xargs perl -pi -e "s/\.js\//js\//sig;"
-find * -name '*.html' | xargs perl -pi -e "s/\.icons\//icons\//sig;"
-
-cd ${GH_PATH}
-
-git add .
-
-git commit -am"Updated test coverage"
 
 git push origin gh-pages
 
